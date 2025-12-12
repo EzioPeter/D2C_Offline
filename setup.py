bd9ce42380138830a4907f39bee0fe35550b319e
XJY <464672479@qq.com>
2025-12-12 11:19:17 +0800

    add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ from setuptools import find_packages
 setup(
     name='d2c',
     description="D2C is a Data-driven Control Library based on reinforcement learning.",
-    url='https://github.com/AIR-DI/D2C.git',
-    python_requires=">=3.7",
+    url='https://github.com/EzioPeter/D2C-XJTU.git',
+    python_requires=">=3.11",
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'gym==0.23.1',
         'numpy',
         'pandas',
-        'torch',
+        'torch==2.4.0',
         'tqdm',
         'json5',
         'wandb',
@@ -23,8 +23,9 @@ setup(
         'easydict',
         'h5py',
         'fire',
+        'tyro',
+        'ml_collections==0.1.1',
+        'Cython==0.29.33',
+        'mujoco-py==2.1.2.14',
     ],
-    extras_require={
-        'mujoco': ['mujoco-py==2.1.2.14']
-    },
 )
